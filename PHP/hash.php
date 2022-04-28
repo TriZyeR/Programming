88c5500e83a534e7b889a9626e30d6d43acef378
TriZyeR <yobachu@gmail.com>
2022-04-28 09:38:54 +0200

    Test

--- PHP/hash.php
+++ PHP/hash.php
@@ -6,13 +6,13 @@
   </head>
   <body>
     <?php
-      echo "test123";
+      echo "test1234";
       echo "<br>";
-      echo password_hash("test123asdasdasdgre321453:;[[asdf]]", PASSWORD_DEFAULT);
+      echo password_hash("test1234asdasdasdgre321453:;[[asdf]]", PASSWORD_DEFAULT);
       echo "<br>";
-      echo password_hash("test123", PASSWORD_DEFAULT);
+      echo password_hash("test1234", PASSWORD_DEFAULT);
 
-     /*$input = "test123";
+     /*$input = "test1234";
      $hashedpassword = password_hash("test123", PASSWORD_DEFAULT);
 
      echo password_verify($input, $hashedpassword);
